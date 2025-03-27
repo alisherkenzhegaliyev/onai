8af892cf2e621290cf88605f8b1d8fed08d430ce
Nurym Alibekov <nurymw.a@gmail.com>
2025-03-27 17:36:18 +0500

    first model

--- back/user-service/migrate/migrate.go
+++ back/user-service/migrate/migrate.go
@@ -1,2 +1,12 @@
 package migrate
 
+import (
+	"github.com/alisherkenzhegaliyev/onai/initializers"
+	"github.com/alisherkenzhegaliyev/onai/models"
+)
+
+func Migrate() {
+	initializers.DB.AutoMigrate(&models.Profile{}, &models.Language{})
+	// other models
+	// ...
+}
