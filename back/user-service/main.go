c4f773ed0c7ef7db437d5a42debb7974c2f5845d
Nurym Alibekov <nurymw.a@gmail.com>
2025-03-31 18:09:06 +0500

    changing everything

--- back/user-service/main.go
+++ back/user-service/main.go
@@ -3,14 +3,12 @@ package main
 import (
 	"github.com/alisherkenzhegaliyev/onai/controllers"
 	"github.com/alisherkenzhegaliyev/onai/initializers"
-	"github.com/alisherkenzhegaliyev/onai/migrate"
 	"github.com/gin-gonic/gin"
 )
 
 func init() {
 	initializers.LoadEnv()
 	initializers.ConnectToDB()
-	migrate.Migrate()
 }
 
 func main() {
