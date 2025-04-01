cd0ff0d06948f42cf2c0bf73d900bc00440d3625
[Islam Tazhiyev] <[islam.tazhiyev@nu.edu.kz]>
2025-04-01 16:58:28 +0500

    testing section added

--- back/user-service/models/testing.go
+++ back/user-service/models/testing.go
@@ -1,2 +1,19 @@
 package models
 
+type Test struct {
+	ID   uint   `json:"id"`
+	Name string `json:"name"`
+}
+
+type UserTest struct {
+	ID     uint `json:"id"`
+	UserID uint `json:"user_id"`
+	TestID uint `json:"test_id"`
+}
+
+type TestDetail struct {
+	ID         uint   `json:"id"`
+	UserTestID uint   `json:"user_test_id"`
+	FieldName  string `json:"field_name"`
+	FieldValue string `json:"field_value"`
+}
